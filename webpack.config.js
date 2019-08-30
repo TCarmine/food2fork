ff021115c094be92f6401f5ddd2bf36546bc6a54
TCarmine <kappa85t@hotmail.it>
2019-08-30 19:11:14 +0200

    some basic structure components

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var HtmlWebpackPlugin =  require('html-webpack-plugin');
 
 module.exports = {
-    entry : './app/index.js',
+    entry : './app/src/index.js',
     output : {
         path : path.resolve(__dirname , 'dist'),
         filename: 'index_bundle.js'
@@ -16,7 +16,7 @@ module.exports = {
     mode:'development',
     plugins : [
         new HtmlWebpackPlugin ({
-            template : 'app/index.html'
+            template : 'app/src/index.html'
         })
     ]
 
