a8fa56dae10bc31a850bbee93191e49183b1f69d
TCarmine <kappa85t@hotmail.it>
2019-08-31 09:34:38 +0200

    added state, method and console to index.js, added some dependencies

--- app/src/index.js
+++ app/src/index.js
@@ -6,14 +6,34 @@ import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 class App extends Component{
+    state = {
+        url:"https://www.food2fork.com/api/search?key=bbdf4b18758a4bccc790efded3977235",
+        recipes:[]  
+    };
+    async getRecipes(){
+       try{ const daterep = await fetch(this.state.url);
+         const jsonData = await data.json();
+         this.setState({
+            recipes:jsonData.recipes
+         });
+       }
+       catch (error){
+          console.log(error);
+       }
+    }
+  
+    componentDidMount(){
+        this.getRecipes()
+    }   
     render(){
-        return(
+       console.log(this.state.recipes);
+         return(
             <React.Fragment>
                     <RecipeList />
                                    
                     <RecipeDetails />    
             </React.Fragment>
-        );
+         );
     }
 }
 
