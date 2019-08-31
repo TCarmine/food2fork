a7597d8669e5d35e8e095b306a32333e872d7c9b
TCarmine <kappa85t@hotmail.it>
2019-08-31 16:04:11 +0200

    installed flow things to type check  js, disabled typescript plugings and fix sintax issue in babel.config

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,16 @@
 module.exports = function (api) {
     api.cache(true);
-    presets: [ "@babel/preset-env","@babel/preset-react"  
-       {
-          useBuiltIns: "usage",
-          corejs: 3
-       }],
-    plugins: [ 
+    const presets= [
+        [ 
+            "@babel/preset-env",
+              {
+                    useBuiltIns: "usage",
+                    corejs: 3
+              }
+         ],
+         "@babel/preset-react",
+    ];  
+    const plugins = [ 
         ["@babel/transform-runtime", 
             {
                   corejs: 3,
@@ -13,9 +18,9 @@ module.exports = function (api) {
         ],
     "@babel/plugin-transform-arrow-functions", 
     "@babel/plugin-proposal-class-properties", 
-   ]
-   return {
-    presets,
-    plugins,
-  }
+   ];
+    return {
+        presets,
+        plugins,
+    };
   }
