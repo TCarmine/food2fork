49bc89cbc67022b92c155210829e58d4afb3478b
TCarmine <kappa85t@hotmail.it>
2019-08-31 08:54:04 +0200

    added some h1

--- app/src/components/RecipeDetails.js
+++ app/src/components/RecipeDetails.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {recipes} from '/home/ca/Desktop/code/food2fork/app/src/tempDetails.js';
+
 
 export default class RecipeDetails extends Component {
     render() {
         return (
     <React.Fragment>
-        hello From Details
+        <h1>hello From Details</h1>
     </React.Fragment>
         )
     }
