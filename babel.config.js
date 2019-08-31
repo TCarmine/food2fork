c24f3959eb06edb2bba10109a6072e2dd32b4ec5
TCarmine <kappa85t@hotmail.it>
2019-08-31 13:15:38 +0200

    added  corejs: 3 in babel.config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-    presets: [ "@babel/preset-env", "@babel/preset-react" ],
+    presets: [ "@babel/preset-env", 
+       {
+          useBuiltIns: "usage",
+          corejs: 3,
+       }, 
+       "@babel/preset-react" ],
     plugins: [ "@babel/plugin-transform-arrow-functions", "@babel/plugin-proposal-class-properties" ]
   }
