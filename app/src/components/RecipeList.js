b1c03c9973aaeea171efd9d135aad329a2144b50
TCarmine <kappa85t@hotmail.it>
2019-09-01 22:24:19 +0200

    RecipeList last

--- app/src/components/RecipeList.js
+++ app/src/components/RecipeList.js
@@ -4,42 +4,48 @@ import RecipeSearch from './RecipeSearch';
 import { localRecipes } from '../tempList';
 
 export default class RecipeList extends Component {
-    
     render() {
-        const { 
-            recipelist,
-            handleDetails,
-            value,
-            handleSubmit,
-            handleChange
-        } = this.props;
-        return (
-            <React.Fragment>
-           
-            <RecipeSearch 
-               value={value} 
-               handleChange={handleChange} 
-               handleSubmit={handleSubmit}
-            />  
-            <div className="container my-5" />
+      const {
+        recipes,
+        handleDetails,
+        value,
+        handleSubmit,
+        handleChange,
+        error
+      } = this.props;
+  
+      return (
+        <React.Fragment>
+          <RecipeSearch
+            value={value}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+          />
+          <div className="container my-5">
             {/* title */}
-            <div className="row">     
-               <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3"> 
-                  <h1 className="text-slanted">Recipes List </h1>
-               </div>
+            <div className="row">
+              <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+                <h1 className="text-slanted">recipe list</h1>
+              </div>
             </div>
             {/* end of title */}
             <div className="row">
-            {
-                recipelist.map(el => {
-                return <Recipe 
-                key={el.recipe_id} 
-                recipe_1={el}
-                handleDetails={handleDetails}/>;
-            })} 
+              {error ? (
+                <h1 className="text-danger text-center">{error}</h1>
+              ) : (
+                recipes.map(recipe => {
+                  return (
+                    <Recipe
+                      key={recipe.recipe_id}
+                      recipe={recipe}
+                      handleDetails={handleDetails}
+                    />
+                  );
+                })
+              )}
             </div>
-      
-            </React.Fragment>
-        );
+          </div>
+        </React.Fragment>
+      );
     }
-}
+  }
